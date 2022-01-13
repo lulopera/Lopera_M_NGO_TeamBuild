e13714743a6d8a180f14f6ebe889a75a8ddeb762
Maria Lopera <marialopera@Marias-MBP.phub.net.cable.rogers.com>
2022-01-12 21:46:19 -0500

    adding routes

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+import express from 'express';
 
-
-import { createServer } from 'http';
-
-const hostname = '127.0.0.1';
+const app = express();
 const port = process.env.PORT || 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>This is really easy!</h1>');
-});
+app.get('/', (req, res) => {
+    res.send('hello from express!');
+})
 
-server.listen(port, () => {
+app.get('/maria', (req, res) => {
+    res.send(`this is maria's`);
+})
+
+app.get('/daniela', (req, res) => {
+    res.send(`this is daniela's`);
+})
+
+app.get('/edward', (req, res) => {
+    res.send(`this is edward's`);
+})
+
+app.listen(port, () => {
   console.log(`Server running at port: ${port}/`);
 });
